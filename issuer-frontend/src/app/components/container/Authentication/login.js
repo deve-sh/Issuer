d0f112227e4d76cb463d435427d44251b764ec8c
Devesh Kumar <devesh2027@gmail.com>
2020-03-01 15:19:12 +0530

    Added Forgot Password mode and constants for better controlling.

--- issuer-frontend/src/app/components/container/Authentication/login.js
+++ issuer-frontend/src/app/components/container/Authentication/login.js
@@ -6,12 +6,14 @@ import Input from "../../reusables/Input";
 import Button from "../../reusables/Button";
 
 import constants from "../../../constants";
+import authConstants from "../../../constants/authConstants";
 
 // Images
 
 import identityImage from "../../../files/identity.svg";
 
 const Login = props => {
+	const [authMode, setauthMode] = useState(authConstants.LOGINMODE);
 	const [email, setemail] = useState("");
 	const [password, setpassword] = useState("");
 
@@ -21,8 +23,22 @@ const Login = props => {
 		return () => (document.title = constants.APPNAME);
 	}, []);
 
+	const switchMode = () => {
+		setauthMode(mode => {
+			if (mode === authConstants.LOGINMODE)
+				return authConstants.FORGOTPASSMODE;
+			else return authConstants.LOGINMODE;
+		});
+	};
+
 	const loginUser = event => {
 		event.preventDefault();
+
+		if (authMode === authConstants.LOGINMODE) {
+			// Log user in.
+		} else {
+			// Forgot Password
+		}
 	};
 
 	return (
@@ -30,7 +46,11 @@ const Login = props => {
 			<div className={"fixedcontainer row aligncenter"}>
 				<div className={"col-sm-7"}>
 					<form className={"authform"} onSubmit={loginUser}>
-						<div className={"heading extraweight"}>Login</div>
+						<div className={"heading extraweight"}>
+							{authMode === authConstants.LOGINMODE
+								? authConstants.LOGIN
+								: authConstants.FORGOTPASS}
+						</div>
 						<label>Email</label>
 						<Input
 							className={"form-control"}
@@ -39,27 +59,56 @@ const Login = props => {
 							onChange={e => setemail(e.target.value)}
 							required={true}
 						/>
-						<br />
-						<label>Password</label>
-						<Input
-							className={"form-control"}
-							type={"password"}
-							placeholder={"Enter Your Password"}
-							onChange={e => setpassword(e.target.value)}
-							required={true}
-						/>
+						{authMode === authConstants.LOGINMODE ? (
+							<React.Fragment>
+								<br />
+								<label>Password</label>
+								<Input
+									className={"form-control"}
+									type={"password"}
+									placeholder={"Enter Your Password"}
+									onChange={e => setpassword(e.target.value)}
+									required={true}
+								/>
+							</React.Fragment>
+						) : (
+							""
+						)}
 						<br />
 						<div className={"buttoncontainer"}>
 							<Button
 								className={"btn btn-primary"}
 								type={"submit"}
-								label={"Login"}
-								title={"Login"}
+								label={
+									authMode === authConstants.LOGINMODE
+										? authConstants.LOGIN
+										: authConstants.UPDATEPASS
+								}
+								title={
+									authMode === authConstants.LOGINMODE
+										? authConstants.LOGIN
+										: authConstants.UPDATEPASS
+								}
 							/>
 							&nbsp;&nbsp;
-							<Link className={"btn"} title={"Register"} to={"/"}>
+							<Link className={"btn"} title={"Cancel"} to={"/"}>
 								Cancel
 							</Link>
+						</div>
+						<br />
+						<div className={"alternatemessage"}>
+							<a
+								href={"#"}
+								onClick={e => {
+									e.preventDefault();
+									switchMode();
+								}}
+								className={"accessibility linkbutton"}
+							>
+								{authMode === authConstants.LOGINMODE
+									? authConstants.FORGOTPASS
+									: authConstants.LOGININSTEAD}
+							</a>
 						</div>
 						<hr className={"sectionseparator"} />
 						<div className={"alternatemessage"}>
---
+++ issuer-frontend/src/app/constants/authConstants.js
@@ -0,0 +1,11 @@
+const authConstants = {
+	LOGINMODE: "Login",
+	LOGIN: "Login",
+	REGISTER: "Register",
+	FORGOTPASS: "Forgot Password?",
+	FORGOTPASSMODE: "Forgot",
+	UPDATEPASS: "Update Password",
+	LOGININSTEAD: "Login Instead"
+};
+
+export default authConstants;
