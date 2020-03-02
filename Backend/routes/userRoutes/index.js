eb2b63fb967f4bcb87c9caa23874eac5ecf6848b
Devesh Kumar <devesh2027@gmail.com>
2020-03-02 23:36:21 +0530

    Completed Login Route.

--- Backend/routes/userRoutes/index.js
+++ Backend/routes/userRoutes/index.js
@@ -1,4 +1,7 @@
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+require("dotenv").config();
 
 const constants = require("../../constants");
 const apiConstants = require("../../constants/apiConstants");
@@ -45,7 +48,7 @@ const userRoutes = router => {
 
 			if (!constants.EMAILREG.test(email))
 				return error(res, 400, "Invalid Email.");
-			
+
 			if (password.length < constants.MINPASSLENGTH)
 				return error(res, 400, "Password Too Short!");
 
@@ -85,10 +88,9 @@ const userRoutes = router => {
 
 						// Hashing Password
 
-						try{
+						try {
 							password = bcrypt.hashSync(password, 12);
-						}
-						catch(err){
+						} catch (err) {
 							return INTERNALSERVERERROR(res);
 						}
 
@@ -116,6 +118,37 @@ const userRoutes = router => {
 						});
 					});
 				});
+			});
+		}
+	);
+
+	router.post(
+		`${apiConstants.USERROUTES}${apiConstants.LOGINROUTE}`,
+		(req, res) => {
+			let { email, password } = req.body;
+
+			if (!email || !password) return INCOMPLETEDETAILS(res);
+
+			// Finding the user
+
+			return findUsersByPhoneOrEmail(email, email, (err, users) => {
+				if (err) return INTERNALSERVERERROR(res);
+				else if (!users || users.length <= 0)
+					return error(res, 404, "User Not Found.");
+
+				let user = users[0];
+
+				let jwtPayload = {
+					email: user.email,
+					_id: user._id,
+					institute: user.institute,
+					department: user.department
+				};
+
+				// Encrypting the JSON Web Token.
+				let token = jwt.sign(jwtPayload, process.env.JWT_SECRET);
+
+				return res.json({ token });
 			});
 		}
 	);
